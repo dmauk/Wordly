1ab8ee7643100b7501c5d81c4ab33d75fc45a10b
Dan Mauk <dmaukdev@gmail.com>
2018-08-19 08:23:46 +0000

    Load articles

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import Header from './Header';
+import Home from './Home';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -10,7 +12,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        { this.props.appName }
+        <Header appName={this.props.appName}/>
+        <Home />
       </div>
     );
   }
